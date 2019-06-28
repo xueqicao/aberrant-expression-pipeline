3738d52b353ed5398c440668bc15f54e71060101
Michaela Mueller <mi.mueller@tum.de>
2019-06-28 23:46:46 +0200

    adapted to strand column

--- Scripts/Counting/countReads.R
+++ Scripts/Counting/countReads.R
@@ -4,7 +4,7 @@
 #' wb:
 #'  input:
 #'   - sample_bam: '`sm lambda wildcards: parser.getFilePath(wildcards.sampleID, "rna_assay") `'
-#'   - features: '`sm parser.getProcDataDir() + "/{annotation}/count_ranges.Rds"`'
+#'   - count_ranges: '`sm parser.getProcDataDir() + "/{annotation}/count_ranges.Rds"`'
 #'  output:
 #'   - counts: '`sm parser.getProcDataDir() + "/{annotation}/counts/{sampleID,[^/]+}.Rds"`'
 #'  type: script
@@ -27,32 +27,41 @@ sample_anno <- sample_anno[get(snakemake@config$rna_assay) == sampleID]
 
 count_mode <- sample_anno[, get(snakemake@config$count_mode_column)]
 paired_end <- sample_anno[, get(snakemake@config$paired_end_column)]
-strand_spec <- sample_anno[, get(snakemake@config$strand_column)]
 inter_feature <- sample_anno[, get(snakemake@config$inter_feature_column)]
-preprocess_reads <- sample_anno[, get(snakemake@config$strand_type_column)]
-if (preprocess_reads == "NULL") {
-  preprocess_reads <- NULL
+strand <- sample_anno[, get(snakemake@config$strand_column)]
+
+# infer preprocessing and strand info
+preprocess_reads <- NULL
+if (strand == "yes") {
+  strand_spec <- T
+} else if (strand == "no"){
+  strand_spec <- F
+} else if (strand == "reverse") {
+  preprocess_reads <- invertStrand
+  strand_spec <- T
+} else {
+  stop(paste("invalid strand information", strand))
 }
 
 # read files
 bam_file <- BamFile(snakemake@input$sample_bam, yieldSize = 2e6)
-feature_regions <- readRDS(snakemake@input$features)
+count_ranges <- readRDS(snakemake@input$count_ranges)
 
 # show info
 message(paste("input:", snakemake@input$features))
 message(paste("output:", snakemake@output$counts))
 message(paste('\tcount mode:', count_mode, sep = "\t"))
 message(paste('\tpaired end:', paired_end, sep = "\t"))
-message(paste('\tstrand specific:', strand_spec, sep = "\t"))
 message(paste('\tinter.feature:', inter_feature, sep = "\t"))
-message(paste('\tpreprocess.reads:', preprocess_reads, sep = "\t"))
-message(paste(seqlevels(feature_regions), collapse = ' '))
+message(paste('\tstrand:', strand, sep = "\t"))
+message(paste('\tstrand specific:', strand_spec, sep = "\t"))
+message(paste(seqlevels(count_ranges), collapse = ' '))
 
 # start counting
-message("counting")
+message("\ncounting")
 starttime <- Sys.time()
 se <- summarizeOverlaps(
-    feature_regions
+  count_ranges
     , bam_file
     , mode = count_mode
     , singleEnd = !paired_end
@@ -60,7 +69,7 @@ se <- summarizeOverlaps(
     , fragments = F
     , count.mapped.reads = T
     , inter.feature = inter_feature # TRUE, reads mapping to multiple features are dropped
-    , preprocess.reads = get(preprocess_reads)
+    , preprocess.reads = preprocess_reads
     , BPPARAM = MulticoreParam(snakemake@threads)
 )
 saveRDS(se, snakemake@output$counts)
