ba2a67b2edbc0eb7d6e3231e95cc73a63db8ad6a
Vicente <yepez@in.tum.de>
2020-01-29 16:59:13 +0100

    summary pages links updated

--- Scripts/Outrider/AllDatasets.R
+++ Scripts/Outrider/AllDatasets.R
@@ -3,24 +3,25 @@
 #' author: mumichae
 #' wb:
 #'  params:
-#'    - ids: '`sm parser.outrider_ids`'
 #'    - tmpdir: '`sm drop.getMethodPath(METHOD, "tmp_dir")`'
 #'  input:
 #'    - summaries: '`sm expand(config["htmlOutputPath"] + "/AberrantExpression/Outrider/{annotation}/Summary_{dataset}.html",
-#'    annotation=config["geneAnnotation"].keys() , dataset=parser.outrider_ids)`'
+#'    annotation=config["geneAnnotation"].keys() , dataset=config["aberrantExpression"]["groups"])`'
 #' output:
 #'   html_document:
 #'    code_folding: hide
 #'    code_download: TRUE
 #'---
 
-saveRDS(snakemake, file.path(snakemake@params$tmpdir, "outrider_overview.snakemake")  )
+saveRDS(snakemake, file.path(snakemake@params$tmpdir, "outrider_overview.snakemake"))
 # snakemake <- readRDS(".drop/tmp/AE/outrider_overview.snakemake")
 
 
-groups <- names(snakemake@params$ids)
+groups <- snakemake@config$aberrantExpression$groups 
 gene_annotation_names <- names(snakemake@config$geneAnnotation)
 summaries_titles <- paste(gene_annotation_names, groups)
-summaries <- paste('[', summaries_titles ,'](', gsub(snakemake@config$htmlOutputPath, ".", snakemake@input$summaries), ')', sep = '')
+summaries <- paste('[', summaries_titles ,'](', 
+                   gsub(snakemake@config$htmlOutputPath, ".", 
+                        snakemake@input$summaries), ')', sep = '')
 summaries <- paste(summaries, sep = '\n')
 #' Summaries: `r summaries`
